45e370185aad98af9f7fc25f74cc88316d3a556c
Paul Phillips <paulp@improving.org>
2012-05-04 17:08:31 -0700

    Making pullreqs work for things which aren't scala.
    % pullreqs
     14  2011-12-07  jsuereth         First cut at debian packaging.
     19  2012-02-21  djspiewak        Respect JAVA_HOME over the PATH configuration (if JAVA_HOME
     23  2012-05-07  leedm777         Fixed group-id for sbt-0.11.3.
     24  2012-05-07  retronym         Handle SBT 0.11.3

--- github-api/src/main/scala/pullreq.scala
+++ github-api/src/main/scala/pullreq.scala
@@ -26,8 +26,8 @@ case class Pull(
   head: Commit,
   user: User,
   title: String,
-  updated_at: String,
-  mergeable: Boolean
+  updated_at: String
+  // mergeable: Boolean
 ) extends Ordered[Pull] {
   def compare(other: Pull): Int = number compare other.number
   def sha10  = head.sha10
@@ -46,12 +46,13 @@ case class Commit(
   def sha10 = sha take 10
 }
 
-object Main {
+class PullReqs(userAndRepo: String) {
   implicit val formats = DefaultFormats // Brings in default date formats etc.
 
-  val urlpath = "http://github.com/api/v2/json/pulls/scala/scala/open"
+  val urlpath = "http://github.com/api/v2/json/pulls/" + userAndRepo + "/open"
+  // val oauth   = "?access_token=" + sys.env("GITHUB_TOKEN")
   val url     = new java.net.URL(urlpath)
-  val req     = slurp(url)
+  val jsonRaw     = slurp(url)
   val json    = parse(slurp(url))
 
   def pulls    = (json \ "pulls").extract[List[Pull]].sorted
@@ -62,28 +63,32 @@ object Main {
   def branches = pulls map (_.branch)
   // def fields(key: String) = (json \\ "head" \\ key children) map (_.extract[String])
   
-  def pp() {
-    Process("jsonpp") #< new StringBufferInputStream(req) !
-  }
+  def pp() = Process("jsonpp") #< new StringBufferInputStream(jsonRaw) !
   def showPulls() {
     pulls foreach {
-      case pull @ Pull(number, Commit(sha, label, ref, repository), user, title, updated_at, mergeable) =>
+      case pull @ Pull(number, Commit(sha, label, ref, repository), user, title, updated_at) =>
         println("%3d  %10s  %-15s  %-60s".format(number, pull.date, user.login, title take 60))
     }
     println(branches.mkString("\ngit merge ", " ", ""))
     println(numbers map ("refs/pull/" + _ + "/head") mkString ("\ngit merge ", " ", ""))
-    // println(shas.mkString("\ngit merge ", " ", ""))
   }
-      
+}
+
+object Main {
   def main(args: Array[String]): Unit = {
-    if (args.isEmpty) {
-      println("Usage: pullreqs <cmds>")
-      println("  Cmds: pp, pulls\n")
-      showPulls()
-    }
-    else args foreach {
-      case "pp"     => pp()
-      case "pulls"  => showPulls()
+    args.toList match {
+      case userAndRepo :: args =>
+        val req = new PullReqs(userAndRepo)
+        println(req.urlpath)
+
+        if (args.isEmpty) req.showPulls()
+        else args foreach {
+          case "pp"     => req.pp()
+          case "pulls"  => req.showPulls()
+        }
+      case args =>
+        println("Usage: pullreqs user/repo [pp | pulls]")
+        println("  Example: pullreqs scala/scala\n")
     }
   }
 }
