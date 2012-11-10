0c88cb4ac2b1aea3a5385ea7a3fd1fcb8cad820d
Jason Zaugg <jzaugg@gmail.com>
2012-11-10 12:14:30 +0100

    Automatic bash completion for bash aliases.
    ~ gl --a
    --abbrev          --abbrev=         --all             --author=
    --abbrev-commit   --after=          --all-match
    
    ~ scala210 -Ylinearizer:
    -Ylinearizer:dfs     -Ylinearizer:normal
    -Ylinearizer:dump    -Ylinearizer:rpo

--- libscala.sh
+++ libscala.sh
@@ -39,9 +39,13 @@ trySource scala-291 scala java github git-aliases git
 # Adding -XX: flags to java
 complete -o default -F _java_with_jvm_opts java
 # Adding -J-XX: flags to scala and friends
-complete -o default -F _scala_with_jvm_opts fsc scalac scala pscalac pscala qscalac qscala scalac29 scala29 scalac3 scala3
+complete -o default -F _scala_with_jvm_opts fsc scalac scala
 # Adding rXXXX svn revision completion to some gh- commands
 complete -F _scala_svn_rev_completion gh-commit gh-svn
 
 # Delete the cached bytecode disassemblies
 alias git-javap-clean='git update-ref -d refs/notes/textconv/javap'
+
+# Install bash completion for all aliases, e.g. `gl --a<TAB>` will complete
+# as per `git log --a<TAB>`.
+source $libscalaBash/complete-alias
